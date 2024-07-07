cdd3dd41ce2c0e898827109da2ba4f4dd797af18
Anshu Maurya <anshu.maurya7300@gmail.com>
2024-07-07 07:51:14 +0000

    datatypes

---
+++ jsbasic/datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version of JavaScript
+
+// alert(3+3)  // Not defined as We are using Node Js, Not Browser
+
+console.log( 3 + 3); console.log("I am Anshu")   // not goot practice.
+// code should be readable should be high and futureproof for better practices
+
+
+// data type
+
+let name = "Anshu"
+let age = 25
+let isLoggedIn = false
+
+// Primitive datatypes
+
+// number => 2 to power 52
+// bigint ( for bigger number)
+// string => ""
+// boolean => true/false
+// null  => it is a standalone value in JS ( null is not undefined, it is empty only eg. let temperature = null ) (datatype of null is Object)
+// undefined => not defined ( eg. let temperature; ) (datatype of undefined is undefined)
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "Anshu");  // String
+console.log(typeof age);      // Number
+console.log(typeof undefined); // Undefined
+console.log(typeof null);     // Object
+
