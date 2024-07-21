8353487cec09dd988ec04262424a9619f10c3992
Anshu Maurya <anshu.maurya7300@gmail.com>
2024-07-21 15:46:17 +0000

    string

--- jsbasic/string.js
+++ jsbasic/string.js
@@ -18,3 +18,32 @@ console.log(myName.length);  // output = 12
 console.log(myName.toUpperCase()); // Output = ANSHU MAURYA  * but this doesn't change the original string   
 console.log(myName.charAt(6));  // Output = M
 console.log(myName.indexOf('M')); // Output = 6
+
+// subString
+const newString = myName.substring(0, 8)
+console.log(newString);  // Output = Anshu Ma
+
+// slice
+const newString1 = myName.slice(2, 4)
+console.log(newString1); // Output = sh   (slice can also take negative values)
+
+// Trim
+const newString2 = "    Please Donn't add more Space     "
+
+console.log(newString2);  // Output =     Please Donn't add more Space     
+console.log(newString2.trim()); // Output = Please Donn't add more Space   *trimmed all extra spaces
+
+// Replace
+const url = "https://www.shriprasadam.shop/"
+
+console.log(url.replace('.shop', '.com')); // Output = https://www.shriprasadam.com/
+
+// includes - to ask anything present in the string
+
+console.log(url.includes('shriprasadam'));  // Output - True
+
+// Split 
+
+console.log(myName.split('-'));
+
+
