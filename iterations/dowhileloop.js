593ca1a9b9d6f81559973695ad030955211ab3d0
Anshu Maurya <anshu.maurya7300@gmail.com>
2024-10-24 17:04:59 +0000

    while, do-while loops

---
+++ iterations/dowhileloop.js
@@ -0,0 +1,29 @@
+// While
+
+let index = 0
+while (index <= 12) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let myArray = ["Anshu", "Princess", "Shaktiman", "Thor", "Superman", "IronMan"]
+
+let array = 0
+while (array <= myArray.length) {
+    console.log(`My Superhero is ${myArray[array]}`);
+    array = array + 1
+}
+
+
+
+// do while  - do work first then check condition after
+
+let score = 1
+
+do {
+    console.log(`my score is ${score}`);
+    score++
+    
+} while (score <= 10);
