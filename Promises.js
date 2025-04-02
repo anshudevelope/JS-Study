12f62ae685efe3329a077b015cd71a2e458c5fbe
Anshu Maurya <anshu.maurya7300@gmail.com>
2025-04-02 17:55:23 +0000

    promise 4

--- Promises.js
+++ Promises.js
@@ -38,3 +38,33 @@ const promiseThree = new Promise(function(resolve, reject){
 promiseThree.then(function(user){
     console.log(user)
 })
+
+// Fourth Promise
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "Anshu", password: "anshu@2025" })
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error) {
+    console.log(error);
+})
+.finally(() => {
+    console.log("The promise is either resolved or rejected");
+})
+
+// Promise 5
