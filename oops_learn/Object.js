481494dbbb2cec8d4a78698d10b47ad1cff9a3a0
Anshu Maurya <anshu.maurya7300@gmail.com>
2025-04-04 07:48:24 +0000

    new & Object

--- oops_learn/Object.js
+++ oops_learn/Object.js
@@ -9,6 +9,8 @@ console.log(multipleBy5(5));
 console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 
+
+
 function createUser(username, score){
     this.username = username
     this.score = score
